92cadd8e07c75430057ed1e7da503a119d6271ea
Kanhav Sukhwal <kanhavsukhwal@gmail.com>
2024-05-01 23:53:46 +0530

    removed unecessary urls

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,10 +1,1 @@
-const config = {
-  development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
-  },
-  production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
-  },
-};
 
-export default config;
