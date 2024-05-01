bcbcae2a2e23632424a07e167a99ce5c7b26471a
Kanhav Sukhwal <kanhavsukhwal@gmail.com>
2024-05-01 23:42:01 +0530

    minor fixes

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -35,91 +35,13 @@ const Customizer = () => {
         return <ColorPicker />;
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
-      case "aipicker":
-        return (
-          <AIPicker
-            prompt={prompt}
-            setPrompt={setPrompt}
-            generatingImg={generatingImg}
-            handleSubmit={handleSubmit}
-          />
-        );
+
       default:
         return null;
     }
   };
   const [imageData, setImageData] = useState(null); // State to hold image data
-  //handle submit button function
-  const handleSubmit = async (type) => {
-    if (!prompt) return alert("Please enter a prompt");
 
-    const url = "https://ai-color-generator.p.rapidapi.com/generate-color";
-    const options = {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": "46a2c7df55msh625eb4f0ed02452p15546fjsndab5aa3fdc39",
-        "X-RapidAPI-Host": "ai-color-generator.p.rapidapi.com",
-      },
-      body: JSON.stringify({
-        colorList: ["#FBE18F", "#1F271B"],
-      }),
-    };
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json(); // Parse JSON response
-      console.log(data); // Verify the response structure
-
-      // Check if images are present in the response
-      // if (data.images && data.images.length > 0) {
-      //   setImageData(data.images[0]); // Update state with image data
-      // }
-      // handleDecals(type, imageData);
-    } catch (error) {
-      console.error(error);
-    }
-
-    // try {
-    //   setGeneratingImg(true);
-
-    //   const deepAIOptions = {
-    //     method: "POST",
-    //     url: "https://ai-text-to-image-generator-api.p.rapidapi.com/realistic",
-    //     headers: {
-    //       "content-type": "application/json",
-    //       "X-RapidAPI-Key":
-    //         "46a2c7df55msh625eb4f0ed02452p15546fjsndab5aa3fdc39",
-    //       "X-RapidAPI-Host": "ai-text-to-image-generator-api.p.rapidapi.com",
-    //     },
-    //     data: {
-    //       inputs: prompt,
-    //     },
-    //   };
-
-    //   // Make a request to DeepAI API using Axios
-    //   const deepAIResponse = await axios.request(deepAIOptions);
-    //   // const response = await fetch("http://localhost:8080/api/v1/dalle", {
-    //   //   method: "POST",
-    //   //   headers: {
-    //   //     "Content-Type": "application/json",
-    //   //   },
-    //   //   body: JSON.stringify({
-    //   //     prompt,
-    //   //   }),
-    //   // });
-
-    //   // const data = await response.json();
-
-    //   console.log(deepAIResponse);
-
-    //   // handleDecals(type, `data:image/png;base64,${data.photo}`);
-    // } catch (error) {
-    //   alert(error);
-    // } finally {
-    //   setGeneratingImg(false);
-    //   setActiveEditorTab("");
-    // }
-  };
   //function to change the decal value in the state
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
