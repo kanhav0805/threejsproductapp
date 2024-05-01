78db94739d761a5fd5c6d721c1e53285f4897644
Kanhav Sukhwal <kanhavsukhwal@gmail.com>
2024-05-01 18:52:30 +0530

    new changes

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { easing } from "maath";
 import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
-
+//new
 const Backdrop = () => {
   const shadows = useRef();
 
